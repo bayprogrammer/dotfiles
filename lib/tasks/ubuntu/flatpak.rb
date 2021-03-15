f97e89707c11b324e2429b3669e2db739d0c31a0
Zeb DeOs <zebdeos@bayprogrammer.com>
2021-03-14 19:08:12 -0700

    Simplify Flatpak recipe

--- lib/tasks/ubuntu/flatpak.rb
+++ lib/tasks/ubuntu/flatpak.rb
@@ -1,11 +1,27 @@
 namespace :ubuntu do
   desc 'setup flatpak & apps'
-  task flatpak: %w(
-    base
-    flatpak:setup
-    flatpak:signal
-    flatpak:discord
-    flatpak:steam
-  )
-end
+  task :flatpak do
+    flatpaks = %w(
+      org.chromium.Chromium
+      com.discordapp.Discord
+      org.gnome.Epiphany
+      org.signal.Signal
+      com.valvesoftware.Steam
+    )
 
+    script do
+      Utils.unless_exists('/usr/bin/flatpak') do |_pathname|
+        sudo :apt, :install, '-y', :flatpak
+        flatpak '--user', 'remote-add', '--if-not-exists', :flathub, 'https://flathub.org/repo/flathub.flatpakrepo'
+
+        puts "You ought to reboot if you just installed Flatpak."
+        puts "Don't forget to re-run the bootstrap process!"
+        exit 0
+      end
+
+      flatpaks.each do |pak|
+        flatpak '--user', :install, '-y', pak
+      end
+    end
+  end
+end
