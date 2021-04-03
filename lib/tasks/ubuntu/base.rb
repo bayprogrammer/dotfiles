64234008866f75eae2291dda309d747aedac4403
Zeb DeOs <zebdeos@bayprogrammer.com>
2021-04-03 15:47:17 -0700

    Install some wallpaper

--- lib/tasks/ubuntu/base.rb
+++ lib/tasks/ubuntu/base.rb
@@ -34,6 +34,28 @@ namespace :ubuntu do
       gauche-doc
       guile-3.0
       guile-3.0-doc
+
+      ubuntu-wallpapers-karmic
+      ubuntu-wallpapers-lucid
+      ubuntu-wallpapers-maverick
+      ubuntu-wallpapers-natty
+      ubuntu-wallpapers-oneiric
+      ubuntu-wallpapers-precise
+      ubuntu-wallpapers-quantal
+      ubuntu-wallpapers-raring
+      ubuntu-wallpapers-saucy
+      ubuntu-wallpapers-trusty
+      ubuntu-wallpapers-utopic
+      ubuntu-wallpapers-vivid
+      ubuntu-wallpapers-wily
+      ubuntu-wallpapers-xenial
+      ubuntu-wallpapers-yakkety
+      ubuntu-wallpapers-zesty
+      ubuntu-wallpapers-artful
+      ubuntu-wallpapers-bionic
+      ubuntu-wallpapers-cosmic
+      ubuntu-wallpapers-disco
+      ubuntu-wallpapers-eoan
     )
 
     script do
